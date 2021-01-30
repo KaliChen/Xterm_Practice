9c215f94e7f6a8ff8f59f4a2cea5588a8793009c
KaliChen <huiyichen19830120@gmail.com>
2021-01-29 21:10:54 -0800

    20210130

--- runMake.py
+++ runMake.py
@@ -5,7 +5,7 @@ import pexpect
 make_child = pexpect.spawn('ssh '+sys.argv[2]+'@'+sys.argv[1])
 make_child.expect ('password:')
 make_child.sendline (sys.argv[3])
-server_child.sendline('cd Desktop/Xterm_Practice/TestSocket')
+make_child.sendline('cd Desktop/Xterm_Practice/TestSocket')
 #print(gZCchild.before)   # Print the result of the ls command.
 make_child.sendline('make')
 print(make_child.before)
