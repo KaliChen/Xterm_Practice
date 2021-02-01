664ec635ab8b5d37c034f06fc609104c91de06fa
KaliChen <huiyichen19830120@gmail.com>
2021-02-01 01:03:07 -0800

    20210201

--- runMakeClean.py
+++ runMakeClean.py
@@ -6,7 +6,7 @@ import pexpect
 makeclean_child = pexpect.spawn('ssh '+sys.argv[2]+'@'+sys.argv[1])
 makeclean_child.expect ('password:')
 makeclean_child.sendline (sys.argv[3])
-makeclean_child.sendline('cd Desktop/Xterm_Practice/TestSocket')
+makeclean_child.sendline('cd ~/Xterm_Practice/TestSocket')
 makeclean_child.sendline('make clean')
 print(makeclean_child.before)
 makeclean_child.interact()
